af2866cc965dd95858d193a4d8ab0b1759581ff3
Mariam <daffmariam8@gmail.com>
2025-04-10 12:02:55 +0200

    route Profil_Users

--- microblogging_app/app/Http/Controllers/ProfilUsersController.php
+++ microblogging_app/app/Http/Controllers/ProfilUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Profil_Users;
+use Carbon\Carbon;
 
 use Illuminate\Http\Request;
 
@@ -13,5 +14,60 @@ class ProfilUsersController extends Controller
             $profilusers = Profil_Users::with('user')->get();
             // dd(asset("storage/photos/Table-1.png"));
             return response()->json($profilusers);
+         }
+
+         public function store(Request $request)
+         {
+            $Profil_Users = new Profil_Users();
+            $Profil_Users->user_id = $request->user_id;
+            $Profil_Users->biographie = $request->biographie;
+            $Profil_Users->created_at = Carbon::now()->toDateTimeString();
+            $Profil_Users->save();
+
+            return response()->json(['message' => 'Profil_Users ajoute'],200);
+         }
+
+         /**
+          * Display the specified resource.
+          */
+         public function show(string $id)
+         {
+            $Profil_Users = Profil_Users::with('user')->findOrFail($id);
+            if(!empty($Profil_Users)){
+             return response()->json($Profil_Users);}
+            else{
+             return response()->json(['message' => 'Profil_Users introuvable'],404);
+            }
+         }
+
+         /**
+          * Update the specified resource in storage.
+          */
+         public function update(Request $request, string $id)
+         {
+             if(Profil_Users::where('id', $id)->exists()){
+                 $Profil_Users = Profil_Users::find($id);
+                 $Profil_Users->user_id = $request->user_id;
+                $Profil_Users->biographie = $request->biographie;
+                 $Profil_Users->updated_at = Carbon::now()->toDateTimeString();
+                 $Profil_Users->save();
+                 return response()->json(['message' => 'Profil_Users modifie'],200);
+             }else{
+                 return response()->json(['message' => 'Profil_Users introuvable'],404);
+             }
+         }
+
+         /**
+          * Remove the specified resource from storage.
+          */
+         public function destroy(string $id)
+         {
+             if(Profil_Users::where('id', $id)->exists()){
+                 $Profil_Users = Profil_Users::find($id);
+                 $Profil_Users->delete();
+                 return response()->json(['message' => 'Profil_Users supprimé']);
+             }else{
+                 return response()->json(['message' => 'Profil_Users introuvable'],404);
+             }
          }
 }
--- microblogging_app/routes/api.php
+++ microblogging_app/routes/api.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 
 
 Route::get("/Users", [UserController::class, 'index']);
-Route::get("/Profil", [ProfilUsersController::class, 'index']);
+
 
 
 Route::get("/Posts", [PostController::class, 'index']);
@@ -18,3 +18,9 @@ Route::post("/Posts", [PostController::class, 'store']);
 Route::get("/Posts/{id}", [PostController::class, 'show']);
 Route::put("/Posts/{id}", [PostController::class, 'update']);
 Route::delete("/Posts/{id}", [PostController::class, 'destroy']);
+
+Route::get("/Profil_Users", [ProfilUsersController::class, 'index']);
+Route::post("/Profil_Users", [ProfilUsersController::class, 'store']);
+Route::get("/Profil_Users/{id}", [ProfilUsersController::class, 'show']);
+Route::put("/Profil_Users/{id}", [ProfilUsersController::class, 'update']);
+Route::delete("/Profil_Users/{id}", [ProfilUsersController::class, 'destroy']);
