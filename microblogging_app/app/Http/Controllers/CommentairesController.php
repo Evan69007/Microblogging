660f577ab716c8d7f103df0589b5917a3b610301
Vincent4222 <vincent.bourgin@hotmail.fr>
2025-04-10 12:18:27 +0200

    commentaires

--- microblogging_app/app/Http/Controllers/CommentairesController.php
+++ microblogging_app/app/Http/Controllers/CommentairesController.php
@@ -1,10 +1,80 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Commentaires;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class CommentairesController extends Controller
 {
-    //
-}
+        public function index()
+         {
+            // Fetch all commentaires with their photos
+            $commentaire = Commentaires::with('user')->get();
+            // dd(asset("storage/photos/Table-1.png"));
+            return response()->json($commentaire);
+         }
+
+         public function store(Request $request)
+         {
+            $commentaire = new Commentaires();
+            $commentaire->user_id = $request->user_id;
+            $commentaire->post_id = $request->post_id;
+            $commentaire->description = $request->description;
+          
+            $commentaire->created_at = Carbon::now()->toDateTimeString();
+            $commentaire->save();
+
+            return response()->json(['message' => 'commentaire ajoute'],200);
+         }
+
+         /**
+          * Display the specified resource.
+          */
+         public function show(string $id)
+         {
+            $commentaire = Commentaires::with('user')->findOrFail($id);
+            if(!empty($commentaire)){
+             return response()->json($commentaire);}
+            else{
+             return response()->json(['message' => 'commentaire introuvable'],404);
+            }
+         }
+
+         /**
+          * Update the specified resource in storage.
+          */
+         public function update(Request $request, string $id)
+         {
+             if(Commentaires::where('id', $id)->exists()){
+                 $commentaire = Commentaires::find($id);
+                 $commentaire->user_id = $request->user_id;
+                 $commentaire->post_id = $request->post_id;
+                 $commentaire->description = $request->description;
+                 
+                 $commentaire->updated_at = Carbon::now()->toDateTimeString();
+                 $commentaire->save();
+                 return response()->json(['message' => 'commentaire modifie'],200);
+             }else{
+                 return response()->json(['message' => 'commentaire introuvable'],404);
+             }
+         }
+
+         /**
+          * Remove the specified resource from storage.
+          */
+         public function destroy(string $id)
+         {
+             if(Commentaires::where('id', $id)->exists()){
+                 $commentaire = Commentaires::find($id);
+                 $commentaire->delete();
+                 return response()->json(['message' => 'commentaire supprimé']);
+             }else{
+                 return response()->json(['message' => 'commentaire introuvable'],404);
+             }
+         }
+     }
+
+
+
--- microblogging_app/routes/api.php
+++ microblogging_app/routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfilUsersController;
+use App\Http\Controllers\CommentairesController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Hash;
@@ -18,3 +20,9 @@ Route::post("/Posts", [PostController::class, 'store']);
 Route::get("/Posts/{id}", [PostController::class, 'show']);
 Route::put("/Posts/{id}", [PostController::class, 'update']);
 Route::delete("/Posts/{id}", [PostController::class, 'destroy']);
+
+Route::get("/Commentaires", [CommentairesController::class, 'index']);
+Route::post("/Commentaires", [CommentairesController::class, 'store']);
+Route::get("/Commentaires/{id}", [CommentairesController::class, 'show']);
+Route::put("/Commentaires/{id}", [CommentairesController::class, 'update']);
+Route::delete("/Commentaires/{id}", [CommentairesController::class, 'destroy']);
